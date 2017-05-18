084f232563ff0fe68be2b1e10c4fba17e01e5c6b
Camila Sáez <cfsaezbravo@gmail.com>
2017-05-18 00:07:30 -0400

    Comprobando funciones

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
-function nuevaLista(){
+(function(){
 
-	var añadirLista = document.getElementById("añadir-lista").value;
-	var botoncito = document.getElementById("añadir-titulo");
-	añadirLista.addEventListener("click", function(){
-		alert("Está funcionando");
-	})
-};
+	//Variables
+	var lista = document.getElementById("lista");
+	var tareaInput = document.getElementById("tareaInput");
+	var botonNuevaTarea = document.getElementById("btn-agregar");
+ 
+	//Funciones
+	var agregarTarea = function(){
+		var tarea = tareaInput.value;
+		var nuevaTarea = document.createElement("li");
+		var enlace = document.createElement("a");
+		var contenido = document.createTextNode(tarea);
+ 
+	};
 
-nuevaLista();
+	var comprobarInput = function(){
+		alert("comprobando input");
+	}
+
+	var eliminarTarea = function(){
+		alert("eliminando tareas");
+	}
+ 
+	// Agregar Tarea
+	botonNuevaTarea.addEventListener("click", agregarTarea);
+ 
+	// Comprobar Input
+	tareaInput.addEventListener("click", comprobarInput);
+}());
